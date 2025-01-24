d02b70d6b2e9cef6e0e4452a5b26388ab34bcd88
Alexander-Guguchkin <guguckins72@gmail.com>
2025-01-25 01:52:00 +0300

    Добавление навигации

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,14 @@
 <template>
   <UApp>
-    <NuxtPage />
+    <div class="wrapper">
+      <NavMenu/>
+      <NuxtPage />
+    </div>
   </UApp>
 </template>
+<style scoped>
+.wrapper{
+  display: grid;
+  grid-template-columns: 20% 80%;
+}
+</style>
---
+++ app/components/NavMenu.vue
@@ -0,0 +1,96 @@
+<script setup lang="ts">
+const items = ref([
+  [
+    {
+      label:'Profile',
+      avatar: {
+        src: 'https://avatars.githubusercontent.com/u/739984?v=4'
+      },
+      badge: 100
+    },
+    {
+      label: 'Главная',
+      type: 'label',
+      to:'/'
+    },
+    {
+      label: 'ПК и орг техника',
+      icon: 'i-lucide-book-open',
+      children: [
+      {
+          label: 'Компьютеры',
+          description: 'Fully styled and customizable components for Nuxt.',
+          icon: 'i-lucide-house',
+        },
+        {
+          label: 'Комплектующие',
+          description: 'Fully styled and customizable components for Nuxt.',
+          icon: 'i-lucide-house',
+        },
+
+        {
+          label: 'Мониторы',
+          description: 'Learn how to install and configure Nuxt UI in your application.',
+          icon: 'i-lucide-cloud-download'
+        },
+        {
+          label: 'Гарнитура',
+          icon: 'i-lucide-smile',
+          description: 'You have nothing to do, @nuxt/icon will handle it automatically.'
+        },
+        {
+          label: 'Орг. техника',
+          icon: 'i-lucide-swatch-book',
+          description: 'Choose a primary and a neutral color from your Tailwind CSS theme.'
+        }
+      ]
+    },
+    {
+      label: 'Сервера',
+      icon: 'i-lucide-database',
+      to:'/servers'
+    },
+    {
+      label: 'Пароли',
+      icon: 'i-lucide-box',
+      to:'/password'
+    },
+    {
+      label: 'Прочее',
+      icon: 'i-lucide-box',
+      to:'/password'
+    },
+    {
+      label: 'Пользователи',
+      icon: 'i-lucide-box',
+      to:'/password'
+    },
+    
+  ],
+  [
+    {
+      label: 'Настройки',
+      icon: 'i-simple-icons-github',
+    },
+    {
+      label: 'Help',
+      icon: 'i-lucide-circle-help',
+    }
+  ]
+])
+</script>
+
+<template>
+  <div class="header">
+    <UContainer>
+      <UNavigationMenu orientation="vertical" :items="items" class="data-[orientation=vertical]:w-48" />
+    </UContainer>
+  </div>
+</template>
+<style scoped>
+.header {
+  background: rgb(48, 48, 48);
+  height: 100vh;
+  padding-top: 20px;
+}
+</style>
